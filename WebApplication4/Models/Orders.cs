7f35c68841f962e7b6c913dded621eef37cc90fd
Administrator <Administrator@WINDOWS-UNR7VLH>
2022-08-03 03:23:14 +0530

    Add project files.

---
+++ WebApplication4/Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    
+    public class Orders
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int OrderAmount { get; set; }
+
+    }
+}
